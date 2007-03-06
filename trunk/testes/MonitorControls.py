a607ee02bf7902e0fb4af87ffaa2a1a57fdbc0ec
nuno.a.coelho <nuno.a.coelho@b6e36839-c125-0410-b2e5-9df3a94dcf01>
2007-03-06 00:38:12 +0000

    Continuação da classe MonitorControls.py... -Leitura a partir do ficheiro das propreidades, descrição e nome metodo da classe resizable a executar aquando uma alteração; -Inicio metodo changeProperty;

--- trunk/testes/MonitorControls.py
+++ trunk/testes/MonitorControls.py
@@ -27,95 +27,116 @@ class MonitorControls:
 	TToolBar = "TLB"
 	TTree = "TRE"
 		
-	DefaultValue = "DefaultValue"
+	PropertyValue = "PropertyValue"
 	Description = "Description"
+	
+	deletedControl = -1
+	error = -1
+	true = bool(1)
+	false = bool(0)
 	
 	#MENSAGENS DE ERRO
 	ERROR_OPEN_FILE = "Erro na abertura do ficheiro."
 	
 
 	def __init__(self):
-		#Mapa Associativo para o armazenamento dos controlos		
+		#Mapa Associativo para armazenamento dos controlos		
 		self.DControls = { }
-		#Mapas associativos das propiredades de cada tipo de controlo
+		#Mapa associativo para armazenamento das propriedades dos controlos
 		self.DControlProperties = { }
+		#Mapa associativo para armazenamento da correspondencia entre as propriedades de cada tipo de controlo e os metodos das resizable widget
+		self.DPropertiesMethods = { }		
 		
-		"""self.DEditProperties = {}
-		self.DCheckBoxProperties = {}
-		self.DComboProperties = {}
-		self.DGroupBoxProperties = {}
-		self.DIFrameProperties = {}
-		self.DLabelProperties = {}
-		self.DListProperties = {}
-		self.DMenuBarProperties = {}
-		self.DPasswordFieldProperties = {}
-		self.DRadioButtonProperties = {}
-		self.DSpinnerProperties = {}
-		self.DTabViewProperties = {}
-		self.DTextAreaProperties = {}
-		self.DTextFieldProperties = {}
-		self.DToolBarProperties = {}
-		self.DTreeProperties = {}
-		"""		
-		
-	def haveControls(self, TypeControl):
-		if self.DControls.has_key(TypeControl) == bool(0):
-			return bool(0)
-		else:
-			return bool(1)
-
-	def getNewIDControl(self, TypeControl):
-				
-		if self.haveControls(TypeControl) == bool(0):				
-			return 0
-		else:			
-			for control in self.DControls[TypeControl]:
-				if control == -1:
-					return self.DControls[TypeControl].index(-1)
+		#Ler propriedades
+		self.loadPropertiesFromFile("ControlsDataTypes.dat")
 			
-			return len(self.DControls[TypeControl])
+		
+	def haveTypeControls(self, TypeControl):
+		if self.DControls.has_key(TypeControl) == self.false:
+			return self.false
+		else:
+			return self.true
+	
+	def haveIdControl(self, TypeControl, IdControl):
+		if haveTypeControls and self.DControls[TypeControl].has_key(IdControl):
+			return self.true
+		else:
+			return self.false
+	
+	def getNewIDControl(self, TypeControl):
+
+		if self.haveTypeControls(TypeControl) == self.false:				
+			return 0
+		else:				
+			try:				
+				return self.DControls[TypeControl].values().index(self.deletedControl)			
+			except ValueError:
+				return len(self.DControls[TypeControl].keys())
+
+	
+	def getDefaultProperties(self, TypeControl):
+		
+		if self.DControlProperties.has_key(TypeControl):
+			return self.DControlProperties[TypeControl]
+		return self.deletedControl	
 		
 		
 	def addNewControl(self, TypeControl):
-			
-		Lcontrols = []
+		
+		DTypeControls = {}
+		DTypeControls = self.getDefaultProperties(TypeControl)
+		
+		#verificar se existem propriedades default
+		if DTypeControls == self.error:
+			return self.error
 		
 		#calcular um Id para o control		
-		IDControl = self.getNewIDControl(TypeControl)
+		IdControl = self.getNewIDControl(TypeControl)
+		#adicionar o novo controlo ao mapa
+		DControl = { }
+		DControl[IdControl] = DTypeControls
 		
-		if self.haveControls(TypeControl) == bool(0):
-			Lcontrols.append(IDControl)
-			self.DControls[TypeControl] = Lcontrols			
-		else: #caso no mapa já exista elementos deste tipo de controlo
-			if IDControl == len(self.DControls[TypeControl]):
-				self.DControls[TypeControl].append(IDControl)
-			else:
-				self.DControls[TypeControl][int(IDControl)] = IDControl		
+		#verificar se já existe algum controlo do tipo de dados do controlo a inserir
+		if self.DControls.has_key(TypeControl) == self.false:
+			self.DControls[TypeControl] = { }
 		
-		return IDControl
+		self.DControls[TypeControl].update(DControl)
 		
+		return IdControl
+	
+	
 	def delControl(self, TypeControl, IDControl):
 				
-		if self.haveControls(TypeControl) == bool(0):
-			return -1
-		else:
-			try:	
-				self.DControls[TypeControl].index(IDControl)
-				self.DControls[TypeControl][int(IDControl)] = -1
-				return IDControl
-			except ValueError:
-				return -1
-		
+		if self.haveTypeControls(TypeControl) == self.false:
+			return self.deletedControl
+		else:				
+			if self.DControls[TypeControl].has_key(IDControl) == self.true:
+				self.DControls[TypeControl][IDControl] = self.deletedControl
+			return IDControl
+			
+
 	def getControlsFromType(self, TypeControl):
-		if self.DControls.has_key(TypeControl) == bool(0):
-			return -1
+		if self.haveTypeControls(TypeControl) == self.false:
+			return self.error
 		return self.DControls[TypeControl]
 	
 	def getAllControls(self):
-		print "teste"
-	
-	
-	def loadTypeControlProperties(self, filePath):
+		print self.DControls		
+		
+		
+	def changeProperty(self, TypeControl, IdControl, IdProperty, value):
+		if haveIdControl(TypeControl, IdControl) == self.false:
+			return error
+		else:
+			#Alteração da propreidade na estrutura de dados
+			self.DControls[TypeControl][IdControl][IdProperty][self.PropertyValue] = value
+			
+			#Executar metodo na classe resizable (...)
+			resizablePropertyMethodCall = self.DPropertiesMethods[TypeControl][IdProperty]
+			if len(resizablePropertyMethodCall) > 0: #caso a string na seja vazia
+				print "teste" #(...)
+			
+	def loadTypeControlProperties(self, TypeControl, filePath):
 		
 		DcontrolProperties = { }
 		
@@ -127,10 +148,16 @@ class MonitorControls:
 				line = line.split(':')		
 				IdProperty = line[0]
 				description = line[1]
-				defaultValue = line[2]				
+				defaultValue = line[2]			
+				resizablePropertyMethodCall = line[3]
+				#Armazenar o metodo de chamada à classe resizable no mapa associativo
+				if not self.DPropertiesMethods.has_key(TypeControl):
+					self.DPropertiesMethods[TypeControl] = { }
 				
+				self.DPropertiesMethods[TypeControl][IdProperty] = resizablePropertyMethodCall
+								
 				#Armazenar propriedade no mapa associativo
-				DcontrolProperties[IdProperty] = {self.Description:description, self.DefaultValue: defaultValue}
+				DcontrolProperties[IdProperty] = {self.Description:description, self.PropertyValue: defaultValue}
 				
 			return DcontrolProperties		
 			
@@ -139,8 +166,7 @@ class MonitorControls:
 		
 	
 	def loadPropertiesFromFile(self, filePath):
-		#Abrir ficheiro com a indicação de todos os controlos
-		print str(self.ERROR_OPEN_FILE+" File:"+filePath)
+		#Abrir ficheiro com a indicação de todos os controlos		
 		try:
 			fcontrols=open(filePath)
 			#Ler conteudo do ficheiro
@@ -148,31 +174,34 @@ class MonitorControls:
 				line = line.replace('\n', '')	
 				line = line.split(':')		
 				typeControl = line[0]
-				fileTypeControl = line[1]
+				filePathTypeControl = line[1]
 				
 				#Ler Propriedades do actual tipo de controlo 
-				DcontrolProperties = self.loadTypeControlProperties(fileTypeControl)
+				DcontrolProperties = self.loadTypeControlProperties(typeControl, filePathTypeControl)
 				#Associar ao mapa das caracteristicas dos controlos, as informações sobre as descrições (em forma de mapa associativo)
 				self.DControlProperties[typeControl] = DcontrolProperties				
 		
 		except IOError:
 			str(self.ERROR_OPEN_FILE+" File:"+filePath)
 	
+	
 monitor = MonitorControls()
-"""id0 = monitor.addNewControl(MonitorControls.TEdit)
+id0 = monitor.addNewControl(MonitorControls.TEdit)
 id1 = monitor.addNewControl(MonitorControls.TEdit)
 id2 = monitor.addNewControl(MonitorControls.TEdit)
 id3 = monitor.addNewControl(MonitorControls.TEdit)
-monitor.delControl(MonitorControls.TEdit, id0)
-monitor.delControl(MonitorControls.TEdit, id1)
+#print monitor.getControlsFromType(MonitorControls.TEdit)
+#print monitor.getAllControls()
+monitor.delControl(MonitorControls.TEdit, id2)
 monitor.delControl(MonitorControls.TEdit, id3)
-print monitor.getControlsFromType(MonitorControls.TEdit)
-
 id4 = monitor.addNewControl(MonitorControls.TEdit)
-id5 = monitor.addNewControl(MonitorControls.TEdit)
+print id4
+#id5 = monitor.addNewControl(MonitorControls.TEdit)
 
 print monitor.getControlsFromType(MonitorControls.TEdit)
-"""
 
-monitor.loadPropertiesFromFile("ControlsDataTypes.dat")
-print monitor.DControlProperties
+print monitor.DPropertiesMethods
+
+"""monitor.loadPropertiesFromFile("ControlsDataTypes.dat")
+print monitor.DControlProperties.keys().index('CKB')
+"""
